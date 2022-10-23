da8db666c0a46105c5bb3abf9b8d1715ef3694d6
ndtuan7921 <ndtuan7921@gmail.com>
2022-10-23 21:18:48 +0700

    merge newsfeed

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,22 +1,27 @@
 import React from 'react'
 import Footer from '../../components/Footer/Footer'
+import Header from '../../components/Header/Header'
 import MiniProfile from '../../components/MiniProfile/MiniProfile'
 import Posts from '../../components/Posts/Posts'
 import Suggestion from '../../components/Suggestion/Suggestion'
 
 function Home() {
   return (
-    <main className='max-w-[820px] flex justify-center my-0 mx-auto'>
-      <section className='lg:w-3/4 max-w-[470px]'>
-        <Posts />
-      </section>
+    <>
+      <Header />
+      <main className='max-w-[820px] flex justify-center my-0 mx-auto'>
+        <section className='lg:w-3/4 max-w-[470px]'>
+          <Posts />
+        </section>
 
-      <section className='hidden lg:block w-96 ml-10'>
-        <MiniProfile />
-        <Suggestion />
-        <Footer />
-      </section>
-    </main>
+        <section className='hidden lg:block w-96 ml-10'>
+          <MiniProfile />
+          <Suggestion />
+          <Footer />
+        </section>
+      </main>
+    </>
+
   )
 }
 

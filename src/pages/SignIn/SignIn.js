7e71410fc20ec3898e6d5f5984b338171c244a82
ndtuan7921 <ndtuan7921@gmail.com>
2023-01-17 23:02:40 +0700

    use formik for signIn

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,66 +1,115 @@
-
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState } from "react";
 // import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-import { signIn } from '../../context/authContext/service';
-import { AuthContext } from '../../context/authContext/AuthContext';
+import { signIn } from "../../context/authContext/service";
+import { AuthContext } from "../../context/authContext/AuthContext";
+import * as Yup from "yup";
+import { Formik } from "formik";
 
 function SignIn() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const { dispatch } = useContext(AuthContext)
+  //   const [email, setEmail] = useState("");
+  //   const [password, setPassword] = useState("");
+  const { dispatch } = useContext(AuthContext);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // const authentication = getAuth();
-        // console.log(email, password);
-        // signInWithEmailAndPassword(authentication, email, password)
-        //     .then((response) => {
-        //         console.log(response);
-        //     })
+  const SignInSchema = Yup.object().shape({
+    email: Yup.string()
+      .required("Email required")
+      .matches(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/, "enter an invalid email!"),
+    password: Yup.string().required("Password required"),
+  });
 
-        signIn(dispatch, email, password)
-    }
-    return (
-        <>
-            <div className='pt-9'>
-                <div className="bg-white border border-solid rounded border-ig-elevated-separator w-[350px] flex flex-col items-center mb-3 mx-auto">
-                    <div className='mt-5 mb-3 w-[175px] h-[51px]'>
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png?20160616034027" alt="" />
-                    </div>
-                    <form className='flex flex-col w-full'>
-                        <div className='mt-6 flex flex-col'>
-                            <div className='mx-10 mb-2'>
-                                <input className='box-border text-base w-full bg-ig-secondary-background pt-[9px] pb-[7px] pl-2 text-ig-primary-text border border-solid rounded border-ig-stroke outline-none' value={email}
-                                    onChange={(e) => { setEmail(e.target.value); }} type="text" placeholder='Phone, username or email' />
-                            </div>
-                            <div className='mx-10 mb-2'>
-                                <input className='box-border text-base w-full bg-ig-secondary-background pt-[9px] pb-[7px] pl-2 text-ig-primary-text border border-solid rounded border-ig-stroke outline-none' value={password}
-                                    onChange={(e) => { setPassword(e.target.value) }} type="password" placeholder='Password' />
-                            </div>
-                            <div className='mx-10 my-2'>
-                                <button onClick={handleSubmit} className='w-full cursor-pointer box-border text-sm font-semibold py-[5px] px-[9px] bg-ig-primary-button rounded text-white' type="submit">Log in</button>
-                            </div>
-                            <div className='flex items-center mx-10 mt-2 mb-4 justify-between'>
-                                <div className='w-24 h-[1px] bg-ig-elevated-separator'></div>
-                                <div>OR</div>
-                                <div className='w-24 h-[1px] bg-ig-elevated-separator'></div>
-                            </div>
-                            <div className='text-center mx-10 mb-2 text-[#385185] text-sm font-semibold cursor-pointer'>
-                                <a href="#">Login with facebook</a>
-                            </div>
-                        </div>
-                        <div className='my-5 cursor-pointer text-xs text-center' >
-                            <a href="#">Forgot Password</a>
-                        </div>
-                    </form>
+  //   const handleSubmit = (e) => {
+  //     e.preventDefault();
+  //     // const authentication = getAuth();
+  //     // console.log(email, password);
+  //     // signInWithEmailAndPassword(authentication, email, password)
+  //     //     .then((response) => {
+  //     //         console.log(response);
+  //     //     })
+
+  //     signIn(dispatch, email, password);
+  //   };
+  return (
+    <>
+      <div className="pt-9">
+        <div className="bg-white border border-solid rounded border-ig-elevated-separator w-[350px] flex flex-col items-center mb-3 mx-auto">
+          <div className="mt-5 mb-3 w-[175px] h-[51px]">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png?20160616034027"
+              alt=""
+            />
+          </div>
+          <Formik
+            initialValues={{
+              email: "",
+              password: "",
+            }}
+            validationSchema={SignInSchema}
+            onSubmit={(value) => {
+              signIn(dispatch, value.email, value.password);
+            }}
+          >
+            {({ errors, touched, handleBlur, handleChange, handleSubmit }) => (
+              <form onSubmit={handleSubmit} className="flex flex-col w-full">
+                <div className="mt-6 flex flex-col">
+                  <div className="mx-10 mb-2">
+                    <input
+                      className="box-border text-base w-full bg-ig-secondary-background pt-[9px] pb-[7px] pl-2 text-ig-primary-text border border-solid rounded border-ig-stroke outline-none"
+                      onChange={handleChange}
+                      placeholder="Phone, username or email"
+                      name="email"
+                      onBlur={handleBlur}
+                    />
+                    {errors.email && touched.email && (
+                      <p className="mt-3 text-red-600">{errors.email}</p>
+                    )}
+                  </div>
+                  <div className="mx-10 mb-2">
+                    <input
+                      className="box-border text-base w-full bg-ig-secondary-background pt-[9px] pb-[7px] pl-2 text-ig-primary-text border border-solid rounded border-ig-stroke outline-none"
+                      onChange={handleChange}
+                      placeholder="Password"
+                      name="password"
+                      onBlur={handleBlur}
+                    />
+                    {errors.password && touched.password && (
+                      <p className="mt-3 text-red-600">{errors.password}</p>
+                    )}
+                  </div>
+                  <div className="mx-10 my-2">
+                    <button
+                      className="w-full cursor-pointer box-border text-sm font-semibold py-[5px] px-[9px] bg-ig-primary-button rounded text-white"
+                      type="submit"
+                    >
+                      Log in
+                    </button>
+                  </div>
+                  <div className="flex items-center mx-10 mt-2 mb-4 justify-between">
+                    <div className="w-24 h-[1px] bg-ig-elevated-separator"></div>
+                    <div>OR</div>
+                    <div className="w-24 h-[1px] bg-ig-elevated-separator"></div>
+                  </div>
+                  <div className="text-center mx-10 mb-2 text-[#385185] text-sm font-semibold cursor-pointer">
+                    <a href="#">Login with facebook</a>
+                  </div>
                 </div>
-                <div className="bg-white border border-solid rounded border-ig-elevated-separator w-[350px] flex flex-col items-center py-4 mx-auto">
-                    <p className='text-ig-primary-text text-sm'>Don&#39;t have an account? <a className='text-ig-primary-button font-semibold' href="/signup">Sign up</a></p>
+                <div className="my-5 cursor-pointer text-xs text-center">
+                  <a href="#">Forgot Password</a>
                 </div>
-            </div>
-        </>
-    );
+              </form>
+            )}
+          </Formik>
+        </div>
+        <div className="bg-white border border-solid rounded border-ig-elevated-separator w-[350px] flex flex-col items-center py-4 mx-auto">
+          <p className="text-ig-primary-text text-sm">
+            Don&#39;t have an account?{" "}
+            <a className="text-ig-primary-button font-semibold" href="/signup">
+              Sign up
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default SignIn
-
+export default SignIn;
--- src/pages/Signup/SignUp.js
+++ src/pages/Signup/SignUp.js
@@ -1,119 +1,175 @@
-import React, { useContext } from 'react';
-import * as Yup from 'yup';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
-import FirebaseContext from '../../context/firebase';
+import React, { useContext } from "react";
+import * as Yup from "yup";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import FirebaseContext from "../../context/firebase";
 import { collection, addDoc } from "firebase/firestore"; //getDocs
 
-import { Formik } from 'formik';
+import { Formik } from "formik";
 
 function SignUp() {
-    const SignupSchema = Yup.object().shape({
-        username: Yup.string().required('username required'),
-        email: Yup.string().required('Email required').matches(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/, 'enter an invalid email!'),
-        password: Yup.string().required('Password required').min(4, 'Too short!!!'),
-        confirmPassword: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match').required('Comfirm Password is required!')
+  const SignupSchema = Yup.object().shape({
+    username: Yup.string().required("username required"),
+    email: Yup.string()
+      .required("Email required")
+      .matches(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/, "enter an invalid email!"),
+    password: Yup.string().required("Password required").min(4, "Too short!!!"),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref("password"), null], "Passwords must match")
+      .required("Comfirm Password is required!"),
+  });
 
-    });
+  const { firestore } = useContext(FirebaseContext);
 
-    const { firestore } = useContext(FirebaseContext)
+  return (
+    <>
+      <div className="pt-9">
+        <div className="bg-white border border-solid rounded border-ig-elevated-separator w-[350px] flex flex-col items-center mb-3 mx-auto">
+          <div className="mt-5 mb-3 w-[175px] h-[51px]">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png?20160616034027"
+              alt=""
+            />
+          </div>
+          <Formik
+            initialValues={{
+              email: "",
+              password: "",
+              username: "",
+              confirmPassword: "",
+            }}
+            validationSchema={SignupSchema}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                const authentication = getAuth();
+                createUserWithEmailAndPassword(
+                  authentication,
+                  values.email,
+                  values.password
+                ).then(async () => {
+                  await addDoc(collection(firestore, "users"), {
+                    email: values.email,
+                    username: values.username,
+                    fullname: values.username,
+                    avatar: "default",
+                    number_of_posts: 0,
+                    number_of_followers: 0,
+                    number_of_following: 0,
+                  });
+                });
+                setSubmitting(false);
+              }, 400);
+            }}
+          >
+            {({ errors, touched, handleChange, handleBlur, handleSubmit }) => (
+              <form onSubmit={handleSubmit} className="flex flex-col w-full">
+                <div className="flex flex-col">
+                  <div className="mx-10 mb-2 text-center text-ig-secondary-text font-semibold text-lg">
+                    <p>Sign up to see photos and videos from your friends.</p>
+                  </div>
+                  <div className="mx-10 mb-2 text-center cursor-pointer box-border text-sm font-semibold py-[5px] px-[9px] bg-ig-primary-button rounded text-white">
+                    <a href="#">Log in with facebook</a>
+                  </div>
+                  <div className="flex items-center mx-10 mt-2 mb-4 justify-between">
+                    <div className="w-24 h-[1px] bg-ig-elevated-separator"></div>
+                    <div>OR</div>
+                    <div className="w-24 h-[1px] bg-ig-elevated-separator"></div>
+                  </div>
+                  <div className="mx-10 mb-2">
+                    <input
+                      className="box-border text-base w-full bg-ig-secondary-background pt-[9px] pb-[7px] pl-2 text-ig-primary-text border border-solid rounded border-ig-stroke outline-none"
+                      onChange={handleChange}
+                      name="email"
+                      type="email"
+                      placeholder="Mobile number or email"
+                      onBlur={handleBlur}
+                    />
 
-    return (
-        <>
-            <div className='pt-9'>
-                <div className="bg-white border border-solid rounded border-ig-elevated-separator w-[350px] flex flex-col items-center mb-3 mx-auto">
-                    <div className='mt-5 mb-3 w-[175px] h-[51px]'>
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png?20160616034027" alt="" />
-                    </div>
-                    <Formik
-                        initialValues={{
-                            email: '',
-                            password: '',
-                            username: '',
-                            confirmPassword: ''
-                        }}
-                        validationSchema={SignupSchema}
-                        onSubmit={(values, { setSubmitting }) => {
-                            setTimeout(() => {
-                                const authentication = getAuth();
-                                createUserWithEmailAndPassword(authentication, values.email, values.password)
-                                    .then(async () => {
-                                        await addDoc(collection(firestore, "users"), {
-                                            email: values.email,
-                                            username: values.username,
-                                            fullname: values.username,
-                                            avatar: 'default',
-                                            number_of_posts: 0,
-                                            number_of_followers: 0,
-                                            number_of_following: 0,
-                                        });
-                                    })
-                                setSubmitting(false);
-                            }, 400);
-                        }}>
-                        {({
-                            errors,
-                            touched,
-                            handleChange,
-                            handleBlur,
-                            handleSubmit
-                        }) => (
-                            <form onSubmit={handleSubmit} className='flex flex-col w-full'>
-                                <div className='flex flex-col'>
-                                    <div className='mx-10 mb-2 text-center text-ig-secondary-text font-semibold text-lg'>
-                                        <p>Sign up to see photos and videos from your friends.</p>
-                                    </div>
-                                    <div className='mx-10 mb-2 text-center cursor-pointer box-border text-sm font-semibold py-[5px] px-[9px] bg-ig-primary-button rounded text-white'>
-                                        <a href="#">Log in with facebook</a>
-                                    </div>
-                                    <div className='flex items-center mx-10 mt-2 mb-4 justify-between'>
-                                        <div className='w-24 h-[1px] bg-ig-elevated-separator'></div>
-                                        <div>OR</div>
-                                        <div className='w-24 h-[1px] bg-ig-elevated-separator'></div>
-                                    </div>
-                                    <div className='mx-10 mb-2'>
-                                        <input className='box-border text-base w-full bg-ig-secondary-background pt-[9px] pb-[7px] pl-2 text-ig-primary-text border border-solid rounded border-ig-stroke outline-none'
-                                            onChange={handleChange} name='email' type="email" placeholder='Mobile number or email' onBlur={handleBlur} />
+                    {errors.email && touched.email && (
+                      <p className="mt-3 text-red-600">{errors.email}</p>
+                    )}
+                  </div>
+                  <div className="mx-10 mb-2">
+                    <input
+                      className="box-border text-base w-full bg-ig-secondary-background pt-[9px] pb-[7px] pl-2 text-ig-primary-text border border-solid rounded border-ig-stroke outline-none"
+                      onChange={handleChange}
+                      name="password"
+                      type="password"
+                      placeholder="password"
+                      onBlur={handleBlur}
+                    />
 
-                                        {errors.email && touched.email && <p className='mt-3 text-red-600'>{errors.email}</p>}
-                                    </div>
-                                    <div className='mx-10 mb-2'>
-                                        <input className='box-border text-base w-full bg-ig-secondary-background pt-[9px] pb-[7px] pl-2 text-ig-primary-text border border-solid rounded border-ig-stroke outline-none'
-                                            onChange={handleChange} name='password' type="password" placeholder='password' onBlur={handleBlur} />
+                    {errors.password && touched.password && (
+                      <p className="mt-3 text-red-600">{errors.password}</p>
+                    )}
+                  </div>
+                  <div className="mx-10 mb-2">
+                    <input
+                      className="box-border text-base w-full bg-ig-secondary-background pt-[9px] pb-[7px] pl-2 text-ig-primary-text border border-solid rounded border-ig-stroke outline-none"
+                      onChange={handleChange}
+                      name="username"
+                      type="text"
+                      placeholder="username"
+                      onBlur={handleBlur}
+                    />
 
-                                        {errors.password && touched.password && <p className='mt-3 text-red-600'>{errors.password}</p>}
-                                    </div>
-                                    <div className='mx-10 mb-2'>
-                                        <input className='box-border text-base w-full bg-ig-secondary-background pt-[9px] pb-[7px] pl-2 text-ig-primary-text border border-solid rounded border-ig-stroke outline-none'
-                                            onChange={handleChange} name='username' type="text" placeholder='username' onBlur={handleBlur} />
-
-                                        {errors.username && touched.username && <p className='mt-3 text-red-600'>{errors.username}</p>}
-                                    </div>
-                                    <div className='mx-10 mb-2'>
-                                        <input className='box-border text-base w-full bg-ig-secondary-background pt-[9px] pb-[7px] pl-2 text-ig-primary-text border border-solid rounded border-ig-stroke outline-none'
-                                            onChange={handleChange} name='confirmPassword' type="password" placeholder='Confirm Password' onBlur={handleBlur} />
-                                        {errors.confirmPassword && touched.confirmPassword && <p className='mt-3 text-red-600'>{errors.confirmPassword}</p>}
-                                    </div>
-                                    <div className='mx-10 my-4'>
-                                        <p className='text-xs text-center text-ig-secondary-text'>People who use our service may have uploaded your contact information to Instagram. <a className='font-semibold' href="">Learn More</a></p>
-                                        <p className='text-xs text-center text-ig-secondary-text'>By signing up, you agree to our <a className='font-semibold' href="">Terms , Privacy Policy and Cookies Policy .</a></p>
-                                    </div>
-                                    <div className='mx-10 my-5'>
-                                        <button type="submit" className='w-full cursor-pointer box-border text-sm font-semibold py-[5px] px-[9px] bg-ig-primary-button rounded text-white'>Sign up</button>
-                                    </div>
-
-                                </div>
-
-                            </form>
-                        )}
-                    </Formik>
-
+                    {errors.username && touched.username && (
+                      <p className="mt-3 text-red-600">{errors.username}</p>
+                    )}
+                  </div>
+                  <div className="mx-10 mb-2">
+                    <input
+                      className="box-border text-base w-full bg-ig-secondary-background pt-[9px] pb-[7px] pl-2 text-ig-primary-text border border-solid rounded border-ig-stroke outline-none"
+                      onChange={handleChange}
+                      name="confirmPassword"
+                      type="password"
+                      placeholder="Confirm Password"
+                      onBlur={handleBlur}
+                    />
+                    {errors.confirmPassword && touched.confirmPassword && (
+                      <p className="mt-3 text-red-600">
+                        {errors.confirmPassword}
+                      </p>
+                    )}
+                  </div>
+                  <div className="mx-10 my-4">
+                    <p className="text-xs text-center text-ig-secondary-text">
+                      People who use our service may have uploaded your contact
+                      information to Instagram.{" "}
+                      <a className="font-semibold" href="">
+                        Learn More
+                      </a>
+                    </p>
+                    <p className="text-xs text-center text-ig-secondary-text">
+                      By signing up, you agree to our{" "}
+                      <a className="font-semibold" href="">
+                        Terms , Privacy Policy and Cookies Policy .
+                      </a>
+                    </p>
+                  </div>
+                  <div className="mx-10 my-5">
+                    <button
+                      type="submit"
+                      className="w-full cursor-pointer box-border text-sm font-semibold py-[5px] px-[9px] bg-ig-primary-button rounded text-white"
+                    >
+                      Sign up
+                    </button>
+                  </div>
                 </div>
-                <div className="bg-white border border-solid rounded border-ig-elevated-separator w-[350px] flex flex-col items-center py-4 mx-auto">
-                    <p className='text-ig-primary-text text-sm'>Have an account? <a className='text-ig-primary-button font-semibold' href="/">Log in</a></p>
-                </div>
-            </div>
-        </>
-    )
+              </form>
+            )}
+          </Formik>
+        </div>
+        <div className="bg-white border border-solid rounded border-ig-elevated-separator w-[350px] flex flex-col items-center py-4 mx-auto">
+          <p className="text-ig-primary-text text-sm">
+            Have an account?{" "}
+            <a className="text-ig-primary-button font-semibold" href="/">
+              Log in
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default SignUp
+export default SignUp;

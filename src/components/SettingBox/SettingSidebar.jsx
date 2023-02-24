dfebd215cf5d7d9a2f0adf70df1effde28f82a40
An Le <truonganletk@gmail.com>
2023-02-24 18:46:54 +0300

    account: navlink isActive

--- src/components/SettingBox/SettingSidebar.jsx
+++ src/components/SettingBox/SettingSidebar.jsx
@@ -1,24 +1,20 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export class SettingSidebar extends Component {
   render() {
     return (
       <ul className='border-r border-r-gray-300 basis-3/12 hidden md:block'>
-        <li className='active hover:bg-gray-100'>
-          <Link to={"/accounts/edit"}>
-            <div className='px-5 py-4'>
-              Edit Profile
-            </div>
-          </Link>
-        </li>
-        <li className='active hover:bg-gray-100'>
-          <Link to={"/accounts/changepass"}>
-            <div className='px-5 py-4'>
-              Change Password
-            </div>
-          </Link>
-        </li>
+        <NavLink to={"/accounts/edit"} className={({ isActive }) =>
+          (isActive ? 'border-l border-ig-primary-text ' : '') + ' block w-full px-5 py-4 hover:bg-gray-100'
+        }>
+          Edit Profile
+        </NavLink>
+        <NavLink to={"/accounts/changepass"} className={({ isActive }) =>
+          (isActive ? 'border-l border-ig-primary-text ' : '') + ' block w-full px-5 py-4 hover:bg-gray-100'
+        }>
+          Change Password
+        </NavLink>
       </ul>
     )
   }

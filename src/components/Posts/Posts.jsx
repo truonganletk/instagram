f4c585f2febc0df1f885f5a3658be6ba1dbb5e9f
An Le <truonganletk@gmail.com>
2023-03-09 14:10:52 +0300

    infinite scroll post

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,24 +1,61 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { PostContext } from "../../context/postContext/PostContext";
 import { getLists } from "../../context/postContext/services";
 import Post from "../Post/Post";
 import PostSkeleton from "../Post/PostSkeleton";
 
 function Posts() {
-  const { lists, dispatch, isFetching  } = useContext(PostContext);
+  const [items, setItems] = useState([]);
+  const [postIndex, setPostIndex] = useState(0);
+  const { lists, dispatch, isFetching } = useContext(PostContext);
 
   useEffect(() => {
     getLists(dispatch);
   }, []);
 
-  return (
-    !isFetching ?
+  useEffect(() => {
+    fetchData();
+  }, [lists]);
+
+  const fetchData = () => {
+    // console.log("Fetching data...");
+    // console.log(lists);
+
+    if (postIndex >= lists.length) return;
+    const newItems = [];
+
+    for (let i = postIndex; i < Math.min(postIndex + 1, lists.length); i++) {
+      newItems.push(lists[i]);
+    }
+    
+    setPostIndex(postIndex + 1);
+    setItems([...items, ...newItems]);
+  };
+
+  const onScroll = () => {
+    const scrollTop = document.documentElement.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight;
+    const clientHeight = document.documentElement.clientHeight;
+
+    if (scrollTop + clientHeight >= scrollHeight - 200) {
+      fetchData();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [items]);
+
+  return !isFetching ? (
     <div>
-      {lists?.map((post, index) => (
+      {items?.map((post, index) => (
         <Post key={index} post={post} />
       ))}
-    </div> : 
-    <PostSkeleton/>
+      {items.length < lists.length && <PostSkeleton/>}
+    </div>
+  ) : (
+    <PostSkeleton />
   );
 }
 
